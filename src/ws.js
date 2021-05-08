5df4834c23e8735f23654d5950fb99c935c8bc92
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-05-08 22:10:38 +0900

    create web socket api

--- src/ws.js
+++ src/ws.js
@@ -1,11 +1,21 @@
 import ws from 'ws'
 
-const socket = new ws.Server({ port: 5000 })
+const wss = new ws.Server({ port: 5000 })
 
-socket.on('connection', (ws) => {
+wss.on('connection', (ws) => {
   console.log('connected.')
   ws.send('Hello from server.')
 
-  ws.on('message', (message) => ws.send(`Reply from server: ${message}.`))
+  ws.on('message', (message) => {
+    console.log(`Received: ${message}`)
+
+    if (message === 'Stop Alarm') {
+      wss.clients.forEach((c) => {
+        if (c.readyState === ws.OPEN) c.send(`Stop Alarm!!!`)
+      })
+    } else {
+      ws.send(`Reply from server: ${message}.`)
+    }
+  })
   ws.on('close', () => console.log('Disconnected.'))
 })
