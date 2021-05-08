8c4ce6fe1b4d762a327850581cfe1e18e758e2e4
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-05-08 15:05:19 +0900

    set up linter and formatter

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import { v4 as uuidv4 } from "uuid"
-import mysql from "mysql"
-import express from "express"
+import { v4 as uuidv4 } from 'uuid'
+import mysql from 'mysql'
+import express from 'express'
 //const express = require('express')
 const app = express()
 const port = 3000
@@ -8,25 +8,25 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 //const mysql = require('mysql');
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "express_db", //追加
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'express_db', //追加
 })
 con.connect(function (err) {
   if (err) throw err
-  console.log("Connected")
+  console.log('Connected')
 }) //接続
-app.use(express.urlencoded({ extended: true })) //必須　expressの使い易い形にする
+app.use(express.urlencoded({ extended: true })) //必須 expressの使い易い形にする
 
 //更新
-app.put("/:id", (req, res) => {
+app.put('/:id', (req, res) => {
   //nameを変更している
-  const sql = "UPDATE users set name = ? WHERE id = ?"
+  const sql = 'UPDATE users set name = ? WHERE id = ?'
   con.query(sql, [req.body.name, req.params.id], function (err, result, fields) {
     if (err) throw err
     console.log(result)
-    res.redirect("/")
+    res.redirect('/')
   })
 })
 /*
@@ -43,8 +43,8 @@ app.delete('/:id',(req,res)=>{
 
 //データを入れる
 /*完成*/
-app.post("/users", (req, res) => {
-  const sql = "INSERT INTO users(id,name)VALUES(?,?)"
+app.post('/users', (req, res) => {
+  const sql = 'INSERT INTO users(id,name)VALUES(?,?)'
   const id = uuidv4()
   con.query(sql, [id, req.body.name], function (err, result, fields) {
     if (err) throw err
@@ -57,7 +57,7 @@ app.post("/users", (req, res) => {
 データ入手
 app.get('/', (request, response) => {
 	const sql = "select * from users"
-	con.query(sql, function (err, result, fields) {  
+	con.query(sql, function (err, result, fields) {
 	if (err) throw err;
 	response.send(result)
 	});
