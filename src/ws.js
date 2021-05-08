3971e08a6607d139a05762d8d7573bf7e67c41a6
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-05-08 17:20:39 +0900

    test ws

---
+++ src/ws.js
@@ -0,0 +1,21 @@
+import ws from 'ws'
+
+const server = new ws.Server({ port: 5001 })
+
+// 接続時に呼ばれる
+server.on('connection', (ws) => {
+  // クライアントからのデータ受信時に呼ばれる
+  ws.on('message', (message) => {
+    console.log(message)
+
+    // クライアントにデータを返信
+    server.clients.forEach((client) => {
+      client.send(message)
+    })
+  })
+
+  // 切断時に呼ばれる
+  ws.on('close', () => {
+    console.log('close')
+  })
+})
